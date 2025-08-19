34b911282671cc13328f705269982c19a9b994ee
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-08-19 12:34:28 -0500

    Time: 32 ms (40.46%), Space: 51.4 MB (69.92%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,26 +3,31 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int l = i + 1;
-            int r = nums.length - 1;
 
-            while (l < r) {
-                int sum = nums[l] + nums[r] + nums[i];
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[l], nums[r], nums[i]));
-                    while(l < r && nums[l] == nums[l+1]) l++;
-                    while(l < r && nums[r] == nums[r-1]) r--;
-                    l++;
-                    r--;
-                } else if (sum > 0) {
-                    r--;
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
                 } else {
-                    l++;
+                    k--;
                 }
             }
         }
@@ -30,3 +35,5 @@ class Solution {
         return res;
     }
 }
+
+// [-1,-1,0,1,2,4]
