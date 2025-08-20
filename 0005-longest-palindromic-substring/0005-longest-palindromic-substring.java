c036a4353cea8a1c430eba3e04e34ccf217028f6
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-08-20 10:44:02 -0500

    Time: 14 ms (93.86%), Space: 42.5 MB (58.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,32 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
-        int start = 0;
+        int resLen = 0;
+        int start =0;
         int end = 0;
-        
-        String str= "";
-        String currString = "";
-        
-        for(int i=0; i<s.length(); i++) {
-            int len1 = isPalindrome(s,i,i);
-            int len2 = isPalindrome(s,i,i+1);
-            int maxLen = Math.max(len1,len2);
-            if(maxLen > end-start){
-                start  = i - (maxLen-1)/2;
-                end = i + maxLen/2;
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            int l1 = findLongestPalindrome(i, i, s);
+            int l2 = findLongestPalindrome(i, i+1, s);
+            
+            resLen = Math.max(l1,l2);
+            if(resLen > end-start){
+                start = i - (resLen-1)/2;
+                end = i+resLen/2;
             }
         }
-    return s.substring(start,end+1);
+
+        return s.substring(start, end+1);
     }
-    
-    public int isPalindrome(String s,int fIdx,int sIdx){
-        if(s == null || fIdx >sIdx) return 0;
-        int left = fIdx;
-        int right = sIdx;
-        while(left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right)){
-                left--;
-                right++;
+
+    public int findLongestPalindrome(int l, int r, String s) {
+        
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
         }
-        return right - left - 1; 
+
+        return r-l-1;
     }
 }
