b6c97baec4f1b6f5b822fe295435e6a7e2553678
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-08-21 11:40:45 -0500

    Time: 1 ms (98.09%), Space: 41.2 MB (29.8%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 0;
+        long end = n;
+
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            long curr = mid * (mid+1)/2;
+
+            if(curr == n ){
+                return (int) mid;
+            }else if(curr > n){
+                end = mid-1;
+            }else{
+               start = mid +1; 
+            }
+        }
+
+        return (int)end;
+    }
+}
