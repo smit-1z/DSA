06b00c581b3886de27d722d5686e906af7ca61d2
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-08-21 10:08:54 -0500

    Time: 0 ms (100%), Space: 40.5 MB (47.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,13 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        int[][]dp = new int[m][n];
 
         for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
+            for(int j = n-1;j>=0;j--){
                 if(i == m-1 || j == n-1){
-                    dp[i][j] = 1; 
+                    dp[i][j] = 1;
                 }else{
-                    dp[i][j] = dp[i][j+1] + dp[i+1][j];
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
                 }
             }
         }
