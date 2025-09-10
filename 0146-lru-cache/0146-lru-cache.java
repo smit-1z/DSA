1caecd4894c06994cde5ff77c1a10bc9ed95c1e2
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-09-10 09:31:26 -0500

    Time: 51 ms (27.05%), Space: 117.8 MB (18.94%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,72 +1,67 @@
 class Node {
+    int key;
+    int val;
     Node next;
     Node prev;
-    int val;
-    int key;
 
-    public Node(int key, int val) {
-        this.val = val;
+    Node(int key, int val) {
         this.key = key;
+        this.val = val;
     }
 }
 
 class LRUCache {
     int capacity;
-
     Map<Integer, Node> map;
 
     Node head;
     Node tail;
 
-    public LRUCache(int capacity) {
-        this.map = new HashMap<>();
+    LRUCache(int capacity) {
         this.capacity = capacity;
-
+        this.map = new HashMap<>();
         this.head = new Node(0, 0);
         this.tail = new Node(0, 0);
 
         this.head.next = tail;
         this.tail.prev = head;
+
     }
 
-    public int get(int key) {
-        if (!map.containsKey(key)) {
+    public int get(int key){
+        if(!map.containsKey(key)){
             return -1;
         }
-
-        Node node = map.get(key);
-        remove(node);
-        add(node);
-        return node.val;
+        remove(map.get(key));
+        add(map.get(key));
+        return map.get(key).val;
     }
 
     public void put(int key, int value) {
-        if (map.containsKey(key))
+        if(map.containsKey(key)){
             remove(map.get(key));
+        }
 
-        Node node = new Node(key, value);
+        Node newNode = new Node(key,value);
+        map.put(key,newNode);
+        add(newNode);
 
-        map.put(key,node);
-        add(node);
-
-        if(map.size() >capacity){
+        if(map.size() > capacity){
             map.remove(head.next.key);
             remove(head.next);
         }
     }
 
-    public void add(Node node) {
+    public void add(Node node){
         Node tailPrev = tail.prev;
-
+        tail.prev = node;
         node.next = tail;
         node.prev = tailPrev;
-
         tailPrev.next = node;
-        tail.prev = node;
     }
 
-    public void remove(Node node) {
-        node.prev.next = node.next;
+    public void remove(Node node){
         node.next.prev = node.prev;
+        node.prev.next = node.next;
     }
 }
