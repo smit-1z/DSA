8b2cd32395f84c2fcbaf3a7734920bf1c09eba36
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-09-11 22:12:01 -0500

    Time: 3 ms (98.84%), Space: 43.6 MB (72.04%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -7,15 +7,13 @@ class Solution {
             if (dir.equals(".") || dir.isEmpty()) {
                 continue;
             } else if (dir.equals("..")) {
-                if (index > 0) index--;
+                if(index>0) index--;
             } else {
                 dirs[index++] = dir;
             }
         }
-        if (index == 0) {
+        if (index == 0)
             return "/";
-        }
-
         StringBuilder res = new StringBuilder();
 
         for (int i = 0; i < index; i++) {
