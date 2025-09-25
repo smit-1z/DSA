2d997a9f2330794e3faa7a45e601e3eb94053d98
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-09-25 18:00:32 -0500

    Time: 17 ms (12.91%), Space: 48.1 MB (80.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,32 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
         int max = 0;
-
-        for (int num : nums) {
+        for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
             max = Math.max(max, map.get(num));
         }
 
         List<Integer>[] buckets = new List[max + 1];
 
-        for (int i = 0; i < buckets.length; i++) {
+        for(int i=0;i<buckets.length;i++){
             buckets[i] = new ArrayList<>();
         }
 
-        for (int key : map.keySet()) {
+        for(int key:map.keySet()){
             buckets[map.get(key)].add(key);
         }
 
         int[] res = new int[k];
-        int index = 0;
+        int idx = 0;
 
-        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
-           for(int key:buckets[i]){
-            res[index++] = key;
-           }
-        }       
-        
+        for(int i=buckets.length-1;i>=0;i--){
+            for(int num : buckets[i]){
+                if(idx == k) return res; 
+                res[idx++] = num;
+            }
+        }
         return res;
     }
 }
