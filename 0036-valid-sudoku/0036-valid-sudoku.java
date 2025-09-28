df5e2be7bd5ae5bf8251f430ccf3e0178de95201
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-09-28 18:04:02 -0500

    Time: 3 ms (40.96%), Space: 44.6 MB (55.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,32 +1,32 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<Set<Character>> rowSets = new ArrayList<>();
-        List<Set<Character>> colSets = new ArrayList<>();
-        List<Set<Character>> gridSets = new ArrayList<>();
+        HashMap<Integer, HashSet<Integer>> row = new HashMap<>();
+        HashMap<Integer, HashSet<Integer>> col = new HashMap<>();
+        HashMap<Integer, HashSet<Integer>> grid = new HashMap<>();
 
-        for(int i=0;i<9;i++){
-            rowSets.add(new HashSet<>());
-            colSets.add(new HashSet<>());
-            gridSets.add(new HashSet<>());
+        for(int i =0;i<9;i++){
+            row.put(i,  new HashSet<>());
+            col.put(i,  new HashSet<>());
+            grid.put(i,  new HashSet<>());
         }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] != '.') {
-                    char num = board[i][j];
-                    int grid = (j / 3) + 3 * (i / 3);
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+               if(board[i][j] != '.'){
+                 int c = board[i][j] - '0';
+                int gridKey =(i/3 * 3) + j/3;
 
-                    if (rowSets.get(i).contains(num) || colSets.get(j).contains(num)
-                            || gridSets.get(grid).contains(num)) {
-                        return false;
-                    } else {
-                        rowSets.get(i).add(num);
-                        colSets.get(j).add(num);
-                        gridSets.get(grid).add(num);
-                    }
+                if(row.get(i).contains(c) || col.get(j).contains(c) || grid.get(gridKey).contains(c)){
+                    return false;
                 }
+                
+                row.get(i).add(c);
+                col.get(j).add(c);
+                grid.get(gridKey).add(c);
+               }
             }
         }
+
         return true;
     }
 }
