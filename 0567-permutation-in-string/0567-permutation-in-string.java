c3c4857abb3e8637d6fc233c795ff530c9960d5d
smit joshi <62090827+smit-1z@users.noreply.github.com>
2025-10-07 20:47:20 -0500

    Time: 6 ms (84.34%), Space: 42.4 MB (98.52%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
+        if(s1.length() > s2.length()) return false;
 
-        int[] mapS1 = new int[26];
-        int[] mapS2 = new int[26];
-
-        for (char c : s1.toCharArray()) {
-            mapS1[c - 'a']++;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        for(int i=0;i<s1.length();i++){
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
         }
 
-        for (int i = 0; i < s1.length(); i++) {
-            mapS2[s2.charAt(i) - 'a']++;
+        if(matches(freq1,freq2)) return true;
+
+        for(int i = s1.length();i<s2.length();i++){
+            freq2[s2.charAt(i - s1.length()) - 'a']--;
+            freq2[s2.charAt(i) -'a']++;
+
+            if(matches(freq1,freq2)) return true;
         }
 
-        for (int i = s1.length(); i < s2.length(); i++) {
-            if (checkForPermutation(mapS1, mapS2)) {
-                return true;
-            }
-            mapS2[s2.charAt(i) - 'a']++;
-            mapS2[s2.charAt(i - s1.length()) - 'a']--;
-        }
-
-        return checkForPermutation(mapS1, mapS2);
+        return false;
     }
 
-    public boolean checkForPermutation(int[] mapS1, int[] mapS2) {
-        for (int i = 0; i < mapS1.length; i++) {
-            if (mapS1[i] != mapS2[i]) {
+    public boolean matches(int[] freq1, int[] freq2){
+        for(int i=0;i<26;i++){
+            if(freq1[i] != freq2[i]){
                 return false;
             }
         }
