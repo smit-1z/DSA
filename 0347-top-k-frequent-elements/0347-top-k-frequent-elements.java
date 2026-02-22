c85cf70a9c29f6bf82b16cbf1b50baf08fc0fbbe
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-02-22 17:24:25 -0600

    Time: 17 ms (20.21%), Space: 47.6 MB (82.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,46 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int max = 0;
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            max = Math.max(max, map.get(num));
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        int maxFreq = 0;
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+            maxFreq = Math.max(maxFreq, freqMap.get(num));
         }
 
-        List<Integer>[] buckets = new List[max + 1];
+        List<Integer>[] buckets = new List[maxFreq + 1];
 
-        for(int i=0;i<buckets.length;i++){
+        for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new ArrayList<>();
         }
 
-        for(int key:map.keySet()){
-            buckets[map.get(key)].add(key);
+        for (Integer key : freqMap.keySet()) {
+            buckets[freqMap.get(key)].add(key);
         }
 
         int[] res = new int[k];
-        int idx = 0;
+        int index = 0;
 
-        for(int i=buckets.length-1;i>=0;i--){
-            for(int num : buckets[i]){
-                if(idx == k) return res; 
-                res[idx++] = num;
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            for (int num : buckets[i]) {
+                if (index == k)
+                    return res;
+                res[index++] = num;
             }
         }
         return res;
     }
 }
+/*
+
+{
+    1 3
+    2 2 
+    3 1 
+}
+
+maxfreq = 3 + 1
+
+[ [1] [2] [1] ]
+
+*/
