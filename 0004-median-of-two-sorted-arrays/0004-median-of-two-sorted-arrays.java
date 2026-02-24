aa284c31531a7623d77872b5ba35547d940efd5d
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-02-23 20:25:19 -0600

    Time: 1 ms (99.62%), Space: 48.8 MB (72.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,25 +1,23 @@
 class Solution {
-    int p1 = 0;
-    int p2 = 0;
-
+        int p1 = 0;
+        int p2 = 0;
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
         int size = nums1.length + nums2.length;
 
         if (size % 2 == 0) {
-            for (int i = 0; i < size / 2 - 1; i++) {
-                findNext(nums1, nums2);
+            for (int i = 0; i < (size / 2) -1; i++) {
+                getNext(nums1,nums2);
             }
-            return (findNext(nums1, nums2) + findNext(nums1, nums2)) / 2.0;
+            return (getNext(nums1,nums2) + getNext(nums1,nums2))/2.0;
         } else {
             for (int i = 0; i < size / 2; i++) {
-                findNext(nums1, nums2);
+                getNext(nums1,nums2);
             }
-            return findNext(nums1, nums2);
+            return getNext(nums1,nums2);
         }
     }
 
-    public int findNext(int[] nums1, int[] nums2) {
+    public int getNext(int[] nums1,int[] nums2){
         if(p1 < nums1.length && p2 < nums2.length){
             if(nums1[p1] < nums2[p2]){
                 return nums1[p1++];
@@ -28,7 +26,8 @@ class Solution {
             }
         }else if(p1 < nums1.length){
             return nums1[p1++];
+        }else{
+            return nums2[p2++];
         }
-        return nums2[p2++];
     }
 }
