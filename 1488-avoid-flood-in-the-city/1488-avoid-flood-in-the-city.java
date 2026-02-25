33e51e0655b363823f67f3f4701333c933b177fd
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-02-25 16:54:05 -0600

    Time: 89 ms (87.54%), Space: 164.6 MB (26.7%) - LeetHub

--- 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
@@ -1,32 +1,29 @@
 class Solution {
     public int[] avoidFlood(int[] rains) {
+        Map<Integer,Integer> lastRain = new HashMap<>();
         int n = rains.length;
-
-        int[] res = new int[n];
-
-        Arrays.fill(res, 1);
-
-        Map<Integer, Integer> lastRain = new HashMap<>();
+        int[] res= new int[n];
         TreeSet<Integer> dryDays = new TreeSet<>();
 
-        for (int i = 0; i < n; i++) {
-            if (rains[i] == 0) {
+        for(int i=0;i<n;i++){
+            if(rains[i] == 0){
                 dryDays.add(i);
-            } else {
+                res[i] = 1;
+            }else{
                 int lake = rains[i];
-                if (lastRain.containsKey(lake)) {
-
-                    Integer dryDay = dryDays.higher(lastRain.get(lake));
-                    if (dryDay == null) {
-                        return new int[] {};
+                if(lastRain.containsKey(lake)){
+                    Integer dry = dryDays.higher(lastRain.get(lake));
+                    if(dry == null){
+                        return new int[]{};
                     }
-                    res[dryDay] = lake;
-                    dryDays.remove(dryDay);
+                    res[dry] = lake;
+                    dryDays.remove(dry);
                 }
                 res[i] = -1;
-                lastRain.put(lake, i);
+                lastRain.put(lake,i);
             }
         }
-        return res;
+            return res;
+
     }
 }
