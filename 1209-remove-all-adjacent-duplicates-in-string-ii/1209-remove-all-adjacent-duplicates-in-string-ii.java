51485f0751082c3032a1be083927bce41104a67c
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-02-25 23:44:09 -0600

    Time: 36 ms (32.75%), Space: 48 MB (22.87%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -3,26 +3,27 @@ class Solution {
         Stack<Character> charStack = new Stack<>();
         Stack<Integer> freqStack = new Stack<>();
 
-        for(char c:s.toCharArray()){
-            if(charStack.isEmpty() || c != charStack.peek()) {
+        for (char c : s.toCharArray()) {
+            if (charStack.isEmpty() || c != charStack.peek()) {
                 charStack.push(c);
                 freqStack.push(1);
-            }else if(c == charStack.peek()){
-                    int freq = freqStack.pop();
-                    freq++;
+            } else {
+                int freq = freqStack.pop();
+                freq++;
+                if(freq ==k){
+                    charStack.pop();
+                }else{
                     freqStack.push(freq);
-                    if(!freqStack.isEmpty() && freqStack.peek() == k){
-                        freqStack.pop();
-                        charStack.pop();
-                    }
-        }
+                }
+               
+            }
         }
         StringBuilder res = new StringBuilder();
 
-        while(!charStack.isEmpty()){
+        while (!charStack.isEmpty()) {
             int freq = freqStack.pop();
             char c = charStack.pop();
-            for(int i=0;i<freq;i++){
+            for (int i = 0; i < freq; i++) {
                 res.append(c);
             }
         }
