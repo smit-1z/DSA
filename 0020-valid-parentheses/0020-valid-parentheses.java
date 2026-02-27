686449bf9e9bdbc04022b7bf704c3ac342577867
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-02-26 19:18:16 -0600

    Time: 2 ms (97.67%), Space: 43.5 MB (26.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,17 +5,12 @@ class Solution {
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else {
-                if (c == ')' && (stack.isEmpty() || stack.peek() != '(')) {
-                    return false;
-                }
-                if (c == '}' && (stack.isEmpty() || stack.peek() != '{')) {
-                    return false;
-                }
-                if (c == ']' && (stack.isEmpty() || stack.peek() != '[')) {
-                    return false;
-                }
-                stack.pop();
+            } else if (c == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+                return false;
+            } else if (c == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+                return false;
+            } else if (c == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+                return false;
             }
         }
         return stack.isEmpty();
