2be18d94af60db8fbbf26f801db87bdb85ee09a5
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-02-27 13:59:44 -0600

    Time: 4 ms (62.87%), Space: 43.7 MB (98.48%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,40 +1,35 @@
 class Solution {
-    Map<Integer, List<int[]>> map;
     int min = 0;
     int max = 0;
+    Map<Integer,List<int[]>> map ;
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null)
-            return res;
+        List<List<Integer>>  res = new ArrayList<>();
+        if(root == null) return res;
         this.map = new HashMap<>();
+        buildMap(root,0,0);
 
-        buildMap(root, 0, 0);
-
-        for (int i = min; i <= max; i++) {
+        for(int i=min;i<=max;i++){
             List<int[]> nodesInACol = map.get(i);
-            Collections.sort(nodesInACol, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-            List<Integer> sortedNodesInaCol = new ArrayList<>();
 
-            for (int[] sortedNodes : nodesInACol) {
-                sortedNodesInaCol.add(sortedNodes[1]);
-            }
-            res.add(sortedNodesInaCol);
+            Collections.sort(nodesInACol, (a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+            List<Integer> currLevel = new ArrayList<>();
+
+            for(int[] node: nodesInACol){
+                currLevel.add(node[1]);
+            }            
+            res.add(currLevel);
         }
         return res;
     }
 
-    public void buildMap(TreeNode root, int x, int y) {
-        min = Math.min(min, y);
-        max = Math.max(max, y);
-
-        map.computeIfAbsent(y, k -> new ArrayList<>());
-        map.get(y).add(new int[] { x, root.val });
-
-        if (root.left != null)
-            buildMap(root.left, x + 1, y - 1);
-        if (root.right != null)
-            buildMap(root.right, x + 1, y + 1);
-
+    public void buildMap(TreeNode root,int row, int col){
+        
+        min = Math.min(min, col);
+        max = Math.max(max, col);
+        
+        map.computeIfAbsent(col, k-> new ArrayList<>()).add(new int[]{row, root.val});
+        if(root.left != null) buildMap(root.left,row+1,col-1);
+        if(root.right != null) buildMap(root.right,row+1,col+1);
     }
 }
