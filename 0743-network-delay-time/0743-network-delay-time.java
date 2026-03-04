00ffbf9d72c640751a1d7270234426e262ffddf9
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-04 14:56:21 -0600

    Time: 29 ms (9.58%), Space: 50.8 MB (8.49%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,35 +1,36 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> edges = new HashMap<>();
+        Map<Integer, List<int[]>> adj = new HashMap<>();
 
         for (int[] time : times) {
-            edges.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[] {time[1],time[2]});
+            adj.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[] { time[1], time[2] });
         }
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        minHeap.offer(new int[]{0,k});
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
         Set<Integer> visited = new HashSet<>();
-
         int t = 0;
-        while(!minHeap.isEmpty()){
+
+        minHeap.offer(new int[] { 0, k });
+
+        while (!minHeap.isEmpty()) {
             int[] node = minHeap.poll();
-            int w1 = node[0];
-            int n1 = node[1];
+            int weight = node[0];
+            int curr = node[1];
 
-            if(visited.contains(n1)){
-                continue;
-            }
-            visited.add(n1);
-            t = w1;
+            if (visited.contains(curr)) continue;
+                
+            visited.add(curr);
 
-            if(edges.containsKey(n1)){
-                for(int[] next:edges.get(n1)){
-                    int n2 = next[0];
-                    int w2 = next[1];
-                    minHeap.offer(new int[]{w1+w2,n2});
+            t = weight;
+            
+            if (adj.containsKey(curr)) {
+                for (int[] next : adj.get(curr)) {
+                    int nextNode = next[0];
+                    int nextWeight = next[1];
+                    minHeap.offer(new int[]{nextWeight+weight, nextNode});
                 }
             }
         }
         return visited.size() == n? t:-1;
     }
-
 }
