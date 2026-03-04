f591ec372e9f30a988a640002e38c55038507c16
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-04 16:05:41 -0600

    Time: 121 ms (50.39%), Space: 276.1 MB (20.53%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
@@ -1,48 +1,45 @@
 class Solution {
     public int minCost(int n, int[][] edges) {
-         List<int[]>[] graph = new ArrayList[n];
+        List<int[]>[] graph = new ArrayList[n];
 
-         for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
             graph[i] = new ArrayList<>();
 
-        for(int[] edge:edges){
+        for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
-            graph[u].add(new int[]{u,v,w});
-            graph[v].add(new int[]{v,u,2*w});
+            graph[u].add(new int[] { u, v, w });
+            graph[v].add(new int[] { v, u, w * 2 });
         }
 
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        minHeap.offer(new int[] { 0, 0 }); // cost,nodeID
+
         int[] dist = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-
-        PriorityQueue<int[]> pq =
-            new PriorityQueue<>((a,b)->a[0]-b[0]);
-
-        pq.offer(new int[]{0,0}); // cost, node
-
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
 
-        while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            int cost = cur[0];
-            int node = cur[1];
+        while (!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int cost = curr[0];
+            int node = curr[1];
 
-            if(cost > dist[node]){
+            if (cost > dist[node])
                 continue;
-            }
 
-            for(int[] nei:graph[node]){
-                int next = nei[1];
-                int nextDist = nei[2];
-                int newCost = cost + nextDist;
-                if(newCost < dist[next]){
-                    dist[next] = newCost;
-                pq.offer(new int[]{newCost,next});
+            for (int[] edge : graph[node]) {
+                int next = edge[1];
+                int nextDist = edge[2];
+
+                int newDist = nextDist + cost;
+                if (newDist < dist[next]) {
+                    dist[next] = newDist;
+                    minHeap.offer(new int[] { newDist, next });
                 }
             }
         }
 
-        return dist[n-1] == Integer.MAX_VALUE?-1:dist[n-1];
+        return dist[n - 1] == Integer.MAX_VALUE ? -1 : dist[n - 1];
     }
 }
