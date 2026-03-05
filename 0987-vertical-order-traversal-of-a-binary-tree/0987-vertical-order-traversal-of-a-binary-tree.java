41d4b39a4f7252cc4e32dab282fe210b913381c5
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-05 14:48:03 -0600

    Time: 4 ms (62.7%), Space: 44 MB (78.21%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,35 +1,43 @@
-class Solution {
+
+import java.util.Collection;class Solution {
+    Map<Integer,List<int[]>> map ;
     int min = 0;
     int max = 0;
-    Map<Integer,List<int[]>> map ;
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>>  res = new ArrayList<>();
         if(root == null) return res;
+
         this.map = new HashMap<>();
-        buildMap(root,0,0);
+        dfs(root,0,0);
+
 
         for(int i=min;i<=max;i++){
             List<int[]> nodesInACol = map.get(i);
+            Collections.sort(nodesInACol, (a,b) -> a[0] == b[0] ? a[1] - b[1]: a[0] - b[0]);
+            List<Integer> currentLevel = new ArrayList<>();
 
-            Collections.sort(nodesInACol, (a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-            List<Integer> currLevel = new ArrayList<>();
-
-            for(int[] node: nodesInACol){
-                currLevel.add(node[1]);
-            }            
-            res.add(currLevel);
-        }
-        return res;
+            for(int [] node:nodesInACol){
+                currentLevel.add(node[1]);
+            }
+            res.add(currentLevel);
     }
 
-    public void buildMap(TreeNode root,int row, int col){
-        
+        return res;
+    }
+    public void dfs(TreeNode root, int row, int col){
+
         min = Math.min(min, col);
         max = Math.max(max, col);
-        
-        map.computeIfAbsent(col, k-> new ArrayList<>()).add(new int[]{row, root.val});
-        if(root.left != null) buildMap(root.left,row+1,col-1);
-        if(root.right != null) buildMap(root.right,row+1,col+1);
+
+        map.computeIfAbsent(col, k-> new ArrayList<>()).add(new int[]{row,root.val});
+
+        if(root.left != null){
+            dfs(root.left,row+1,col-1);
+        }
+
+        if(root.right != null){
+            dfs(root.right,row+1,col+1);
+        }
     }
 }
