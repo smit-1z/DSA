55c517eccbee6930823c63ca115a02fd3212b633
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-22 16:38:02 -0500

    Time: 18 ms (47.16%), Space: 46.9 MB (26.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,15 +1,20 @@
 class Solution {
     public String longestPalindrome(String s) {
-            String res = "";
-        for(int i=0;i<s.length();i++){
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
             String a = getLongestLength(s, i, i);
-            String b = getLongestLength(s, i, i+1);
-            if(a.length() >b.length()){
-                if(a.length() > res.length()){
+            String b = getLongestLength(s, i, i + 1);
+
+            if (a.length() > b.length()) {
+                if (a.length() > res.length()) {
                     res = a;
                 }
-            }else{
-                if(b.length() > res.length()){
+            } else {
+                if (b.length() > res.length()) {
                     res = b;
                 }
             }
@@ -18,18 +23,18 @@ class Solution {
         return res;
     }
 
-    public String getLongestLength(String s, int start, int end){
-        
-        while(start>=0 && end<s.length()){
-            if(s.charAt(start) != s.charAt(end)){
-               break;
-            }else{
+    public String getLongestLength(String s, int start, int end) {
+
+        while (start >= 0 && end < s.length()) {
+            if (s.charAt(start) != s.charAt(end)) {
+                break;
+            } else {
                 start--;
                 end++;
             }
         }
-        
-       return s.substring(start+1,end);
+
+        return s.substring(start + 1, end);
     }
 
 }
