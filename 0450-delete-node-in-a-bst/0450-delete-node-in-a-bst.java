e6dffee9bd8604e23f63679347f10cb083212efc
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-28 17:04:41 -0500

    Time: 0 ms (100%), Space: 47.6 MB (47.59%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,30 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return root;
+
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null)
+                return root.right;
+            if (root.right == null)
+                return root.left;
+            else {
+                TreeNode n = root.right;
+
+                while (n.left != null) {
+                    n = n.left;
+                }
+
+                n.left = root.left;
+                TreeNode res = root.right;
+                root = null;
+                return res;
+            }
+        }
+        return root;
+    }
+}
