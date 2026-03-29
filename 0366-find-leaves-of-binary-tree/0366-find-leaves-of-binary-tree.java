abc31d4e8d832840ee1b2351c5b657281f86209b
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-29 13:39:24 -0500

    Time: 0 ms (100%), Space: 43.3 MB (82.59%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
@@ -1,21 +1,25 @@
 class Solution {
-    List<List<Integer>> res;
+    List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> findLeaves(TreeNode root) {
-        this.res = new ArrayList<>();
+        if(root == null) return res;
         dfs(root);
         return res;
     }
 
     public int dfs(TreeNode root){
         if(root == null) return -1;
+
         int l = dfs(root.left);
         int r = dfs(root.right);
 
-        int max = Math.max(l, r) + 1;
+        int max = Math.max(l,r);
 
-        if(max == res.size()) res.add(new ArrayList<>());
-        res.get(max).add(root.val);
+        if(res.size() == max + 1){
+            res.add(new ArrayList<>());
+        }
 
-        return max;
+        res.get(max+1).add(root.val);
+        return max+1;
     }
 }
