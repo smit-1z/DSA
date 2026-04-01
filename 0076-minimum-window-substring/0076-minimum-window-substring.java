96f08888b09703163913ea8a28e2dbe20f52e0a2
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-31 19:18:48 -0500

    Time: 20 ms (38.25%), Space: 47.6 MB (32.36%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,10 +1,10 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character,Integer> tMap = new HashMap<>();
-        Map<Character,Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+        Map<Character, Integer> sMap = new HashMap<>();
 
-        for(char c:t.toCharArray()){
-            tMap.put(c,tMap.getOrDefault(c,0) + 1);
+        for (char c : t.toCharArray()) {
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
 
         int required = tMap.size();
@@ -14,35 +14,33 @@ class Solution {
         int end = 0;
         int resLen = Integer.MAX_VALUE;
         int res[] = new int[2];
-        while(end < s.length()){
+        while (end < s.length()) {
             char c = s.charAt(end);
 
-            sMap.put(c, sMap.getOrDefault(c,0) + 1);
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
 
-            if(tMap.containsKey(c) && tMap.get(c).equals(sMap.get(c))){
+            if (tMap.containsKey(c) && tMap.get(c).equals(sMap.get(c))) {
                 have++;
             }
 
-            while(have == required){
-                char startChar = s.charAt(start);
-                if(resLen >= end - start + 1){
-                    resLen = end-start + 1;
+            while (have == required) {
+                if (resLen > (end - start + 1)) {
+                    resLen = end - start + 1;
                     res[0] = start;
                     res[1] = end;
                 }
+                char startChar = s.charAt(start);
 
-                sMap.put(startChar,sMap.get(startChar) -1);
+                sMap.put(startChar, sMap.get(startChar) - 1);
 
                 start++;
-                if(tMap.containsKey(startChar)){
-                   if(tMap.get(startChar) != sMap.get(startChar)){
+                if (tMap.containsKey(startChar) && sMap.get(startChar) < tMap.get(startChar)) {
                     have--;
-                   }
                 }
             }
             end++;
         }
 
-        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0],res[1] +1);
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
     }
 }
