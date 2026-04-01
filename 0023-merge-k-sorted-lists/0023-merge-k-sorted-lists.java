09eec9459ce0bb0b51229d7748f1e26e0cc83c99
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-31 19:01:03 -0500

    Time: 1 ms (99.95%), Space: 46.8 MB (71.6%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,46 +1,45 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null || lists.length == 0) return null;
+        if (lists == null || lists.length == 0)
+            return null;
 
-        return divideAndConquer(lists,0,lists.length-1);
+        return divideAndConquer(lists, 0, lists.length - 1);
     }
 
-    public ListNode divideAndConquer(ListNode[] lists,int start ,int end){
-        if(start == end){
+    public ListNode divideAndConquer(ListNode[] lists, int start, int end) {
+        if (start == end)
             return lists[start];
-        }
 
-        int mid = start + (end - start)/2;
+        int mid = start + (end - start) / 2;
+        ListNode l1 = divideAndConquer(lists, start, mid);
+        ListNode l2 = divideAndConquer(lists, mid + 1, end);
 
-        ListNode l = divideAndConquer(lists, start, mid);
-        ListNode r = divideAndConquer(lists, mid+1, end);
-
-        return merge(l,r);
+        return merge(l1, l2);
     }
 
-    public ListNode merge(ListNode l1, ListNode l2){
-        ListNode dummyNode = new ListNode(0);
-        ListNode curr = dummyNode;
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
 
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 curr.next = l1;
                 l1 = l1.next;
-            }
-            else{
+            } else {
                 curr.next = l2;
                 l2 = l2.next;
             }
             curr = curr.next;
         }
 
-        if(l1 != null){
+        if (l1 != null) {
             curr.next = l1;
         }
-        if(l2 != null){
+
+        if (l2 != null) {
             curr.next = l2;
         }
-
-        return dummyNode.next;
+        return dummy.next;
     }
 }
