e3c2227fa5f1636375f0ea13676b1df00a14fb79
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-03-31 20:18:57 -0500

    Time: 3 ms (95.06%), Space: 44 MB (83.72%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,43 +1,40 @@
-
-import java.util.Collection;class Solution {
-    Map<Integer,List<int[]>> map ;
+class Solution {
+    Map<Integer, List<int[]>> map = new HashMap<>();
     int min = 0;
     int max = 0;
 
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>>  res = new ArrayList<>();
-        if(root == null) return res;
+    public void buildMap(TreeNode root, int x, int y) {
+        max = Math.max(y, max);
+        min = Math.min(y, min);
 
-        this.map = new HashMap<>();
-        dfs(root,0,0);
-
-
-        for(int i=min;i<=max;i++){
-            List<int[]> nodesInACol = map.get(i);
-            Collections.sort(nodesInACol, (a,b) -> a[0] == b[0] ? a[1] - b[1]: a[0] - b[0]);
-            List<Integer> currentLevel = new ArrayList<>();
-
-            for(int [] node:nodesInACol){
-                currentLevel.add(node[1]);
-            }
-            res.add(currentLevel);
+        map.computeIfAbsent(y, k -> new ArrayList<>());
+        map.get(y).add(new int[] { x, root.val });
+        if (root.left != null)
+            buildMap(root.left, x + 1, y - 1);
+        if (root.right != null)
+            buildMap(root.right, x + 1, y + 1);
     }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+
+        buildMap(root, 0, 0);
+
+        for (int i = min; i <= max; i++) {
+            List<int[]> nodesInCurrentLevel = map.get(i);
+            Collections.sort(nodesInCurrentLevel, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+            List<Integer> currLevel = new ArrayList<>();
+
+            for (int[] node : nodesInCurrentLevel) {
+                currLevel.add(node[1]);
+            }
+
+            res.add(currLevel);
+        }
 
         return res;
-    }
-    public void dfs(TreeNode root, int row, int col){
-
-        min = Math.min(min, col);
-        max = Math.max(max, col);
-
-        map.computeIfAbsent(col, k-> new ArrayList<>()).add(new int[]{row,root.val});
-
-        if(root.left != null){
-            dfs(root.left,row+1,col-1);
-        }
-
-        if(root.right != null){
-            dfs(root.right,row+1,col+1);
-        }
     }
 }
