fb80aa2a66ab5ddff28e90d148d0980c6a5a436d
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-04-04 11:45:44 -0500

    Time: 11 ms (48.53%), Space: 46.6 MB (38.11%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -4,9 +4,9 @@ class Solution {
         int prev = 0;
         int cur = 1;
 
-        for(int i =1;i<s.length();i++){
-            if(s.charAt(i-1) !=s.charAt(i)){
-                ans+=Math.min(prev,cur);
+        for(int i = 1;i< s.length();i++){
+            if(s.charAt(i-1)  != s.charAt(i)){
+                ans += Math.min(prev,cur);
                 prev = cur;
                 cur = 1;
             }else{
@@ -14,6 +14,6 @@ class Solution {
             }
         }
 
-        return ans + Math.min(cur,prev);
+        return ans += Math.min(prev, cur);
     }
 }
