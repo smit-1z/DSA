b63b81ac7efd901cd4bdb1b345380917152235e2
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-04-04 12:13:26 -0500

    Time: 0 ms (100%), Space: 42.8 MB (64.14%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,30 +1,33 @@
 class Solution {
     public String decodeString(String s) {
         Stack<Integer> intStack = new Stack<>();
-        Stack<String> charStack = new Stack<>();
-
+        Stack<String> stringStack = new Stack<>();
         StringBuilder curr = new StringBuilder();
-        int digit = 0;
 
-        for(int i=0;i<s.length();i++){
-            if(Character.isDigit(s.charAt(i))){
-                digit = digit*10 + (s.charAt(i) - '0');
-            }else if(s.charAt(i) == '['){
-                charStack.push(curr.toString());
-                intStack.push(digit);
+        int digit = 0;
+        for(char c:s.toCharArray()){
+            if(Character.isDigit(c)){
+                digit = digit*10 + (c - '0');
+            }
+            else if(c == '['){
+                stringStack.push(curr.toString());
                 curr = new StringBuilder();
+                intStack.push(digit);
                 digit = 0;
-            }else if (s.charAt(i) == ']'){
-                String temp = curr.toString();
-                curr = new StringBuilder(charStack.pop());
-                int freq = intStack.pop();
-                for(int j=0;j<freq;j++){
+            }
+            else if(c == ']'){
+              String temp = curr.toString();
+              curr = new StringBuilder(stringStack.pop());
+
+                int d = intStack.pop();
+
+                for(int i=0;i<d;i++){
                     curr.append(temp);
                 }
             }else{
-                curr.append(s.charAt(i));
+                curr.append(c);
             }
-        } 
+        }
         return curr.toString();
     }
 }
