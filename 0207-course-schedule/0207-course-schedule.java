70b08da889f290e1b618d7d3060db4c9e998292b
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-04-04 11:26:25 -0500

    Time: 7 ms (60.6%), Space: 46.8 MB (90.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,22 +1,21 @@
 class Solution {
-    Set<Integer> visiting = new HashSet<>();
+    Map<Integer, List<Integer>> map = new HashMap<>();
     Set<Integer> visited = new HashSet<>();
-    Map<Integer,List<Integer>> map = new HashMap<>();
-    
+    Set<Integer> visiting = new HashSet<>();
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        for(int i =0;i<numCourses;i++){
-            map.put(i,new ArrayList<>());
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i, new ArrayList<>());
         }
 
-        for(int [] pre:prerequisites){
-            int course = pre[0];
-            int preReq = pre[1];
-            map.get(course).add(preReq);
+        for (int[] preReq : prerequisites) {
+            int course = preReq[0];
+            int pre = preReq[1];
+            map.get(course).add(pre);
         }
 
-        for(int i=0;i<numCourses;i++){
-            if(!visited.contains(i) && cycleFound(i)){
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited.contains(i) && cycleFound(i)) {
                 return false;
             }
         }
@@ -24,22 +23,20 @@ class Solution {
         return true;
     }
 
-    public boolean cycleFound(int courseId){
-        if(visiting.contains(courseId)){
+    public boolean cycleFound(int courseId) {
+        if (visiting.contains(courseId)) {
             return true;
         }
-        if(visited.contains(courseId)){
+
+        if (visited.contains(courseId)) {
             return false;
         }
-
         visiting.add(courseId);
-
-        for(int pre: map.get(courseId)){
-            if(!visited.contains(pre) && cycleFound(pre)){
+        for (int pre : map.get(courseId)) {
+            if (cycleFound(pre)) {
                 return true;
             }
         }
-
         visiting.remove(courseId);
         visited.add(courseId);
         return false;
