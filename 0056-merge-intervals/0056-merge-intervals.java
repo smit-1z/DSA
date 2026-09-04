0acf23bb9ee5cb7e699848c34a1e405deabe110a
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-04 09:13:48 -0500

    Time: 8 ms (91.1%), Space: 49.1 MB (60.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int index = 0;
-        Arrays.sort(intervals, (a,b) -> a[0] -b[0]);
+        if(intervals.length == 0)  return intervals;
+        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
+        ArrayList<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
 
-        for(int[] interval: intervals){
-            if(intervals[index][1] >= interval[0]){
-                intervals[index][1] = Math.max(interval[1],intervals[index][1]);
+        for(int[] interval : intervals){
+            if(res.getLast()[1] >= interval[0]){
+                res.getLast()[1] = Math.max(interval[1],res.getLast()[1]);
             }else{
-                index++;
-                intervals[index] =interval;
+                res.add(interval);
             }
         }
 
-        return Arrays.copyOf(intervals, index+1);
+        return  res.toArray(new int[res.size()][]);
     }
 }
