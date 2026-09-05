8472fe79f677749d321f75fe5470bd265e011f94
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-05 11:46:22 -0500

    Time: 0 ms (100%), Space: 47.8 MB (43.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int maxR = 0;
-        int maxL = 0;
         int l = 0;
         int r = height.length - 1;
-        int maxWater = 0;
-        int water =0;
-        while(l < r){
-            if(height[l] < height[r]){
-                maxL = Math.max(maxL, height[l]);
-                water = maxL - height[l];
+        int lMax = 0;
+        int rMax = 0;
+        
+        int res = 0;
+
+        while (l <= r) {
+            if (height[l] < height[r]) {
+                lMax = Math.max(lMax, height[l]);
+                res += (lMax - height[l]);
                 l++;
-            }else{
-                maxR = Math.max(maxR, height[r]);
-                water = maxR - height[r];
+
+            } else {
+                rMax = Math.max(rMax, height[r]);
+                res += (rMax - height[r]);
                 r--;
             }
-
-            maxWater += water;   
         }
-        return maxWater;
+        return res;
     }
 }
