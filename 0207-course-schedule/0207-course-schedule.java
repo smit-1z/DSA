9fd1e2b5a972f5bc85d94948d74e408a97f261e2
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-05 10:21:08 -0500

    Time: 7 ms (56.51%), Space: 47 MB (64.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,44 @@
 class Solution {
+    Set<Integer> visiting = new HashSet<>();
     Map<Integer, List<Integer>> map = new HashMap<>();
     Set<Integer> visited = new HashSet<>();
-    Set<Integer> visiting = new HashSet<>();
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+
         for (int i = 0; i < numCourses; i++) {
             map.put(i, new ArrayList<>());
         }
 
-        for (int[] preReq : prerequisites) {
-            int course = preReq[0];
-            int pre = preReq[1];
-            map.get(course).add(pre);
+        for (int[] pre : prerequisites) {
+            map.get(pre[0]).add(pre[1]);
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (!visited.contains(i) && cycleFound(i)) {
+            if (!visited.contains(i) && foundCycle(i)) {
                 return false;
             }
         }
-
         return true;
     }
 
-    public boolean cycleFound(int courseId) {
-        if (visiting.contains(courseId)) {
+    public boolean foundCycle(int num) {
+        if (visiting.contains(num)) {
             return true;
         }
-
-        if (visited.contains(courseId)) {
+        if(visited.contains(num)){
             return false;
         }
-        visiting.add(courseId);
-        for (int pre : map.get(courseId)) {
-            if (cycleFound(pre)) {
+
+        visiting.add(num);
+
+        for (int pre : map.get(num)) {
+            if (foundCycle(pre)) {
                 return true;
             }
         }
-        visiting.remove(courseId);
-        visited.add(courseId);
+
+        visiting.remove(num);
+        visited.add(num);
         return false;
     }
 }
