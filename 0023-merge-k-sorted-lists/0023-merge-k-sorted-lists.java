298ed186706916f7b847fc01933bda4d9c166410
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-05 10:41:01 -0500

    Time: 1 ms (99.95%), Space: 46.7 MB (64.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,45 +1,38 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0)
-            return null;
+        if(lists == null || lists.length == 0) return null;
 
         return divideAndConquer(lists, 0, lists.length - 1);
-    }
+     }
 
-    public ListNode divideAndConquer(ListNode[] lists, int start, int end) {
-        if (start == end)
-            return lists[start];
-
-        int mid = start + (end - start) / 2;
-        ListNode l1 = divideAndConquer(lists, start, mid);
-        ListNode l2 = divideAndConquer(lists, mid + 1, end);
-
-        return merge(l1, l2);
+    public ListNode divideAndConquer(ListNode[] lists, int start,int end){
+        if(start == end) return lists[start];
+        int mid = start + (end - start)/2;
+        ListNode l1 = divideAndConquer(lists,start,mid);
+        ListNode l2 = divideAndConquer(lists,mid+1,end);
+        return merge(l1,l2);
     }
 
     public ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-
-        ListNode curr = dummy;
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
 
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                curr.next = l1;
+                temp.next = l1;
                 l1 = l1.next;
             } else {
-                curr.next = l2;
+                temp.next = l2;
                 l2 = l2.next;
             }
-            curr = curr.next;
+            temp = temp.next;
         }
 
         if (l1 != null) {
-            curr.next = l1;
+            temp.next = l1;
+        } else {
+            temp.next = l2;
         }
-
-        if (l2 != null) {
-            curr.next = l2;
-        }
-        return dummy.next;
+        return head.next;
     }
 }
