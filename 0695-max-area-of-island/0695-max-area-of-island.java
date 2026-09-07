90dfb36f5ba9c8fa7831e81bb817651e7b9a2648
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-07 15:12:36 -0500

    Time: 2 ms (63.41%), Space: 46.7 MB (21.7%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,15 +1,16 @@
 class Solution {
-    int maxArea = 0;
-    int[][] seen;
+    boolean[][] covered;
 
     public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+
         int m = grid.length;
         int n = grid[0].length;
-        this.seen = new int[m][n];
+        this.covered = new boolean[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && seen[i][j] == 0) {
+                if (!covered[i][j] && grid[i][j] == 1) {
                     int area = dfs(grid, i, j);
                     maxArea = Math.max(area, maxArea);
                 }
@@ -18,13 +19,15 @@ class Solution {
         return maxArea;
     }
 
-    public int dfs(int[][] grid, int x, int y) {
-        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] != 1 || seen[x][y] == 1) {
+    public int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || covered[i][j] || grid[i][j] != 1) {
             return 0;
         }
-
-        seen[x][y] = 1;
-
-        return 1 + dfs(grid, x, y + 1) + dfs(grid, x, y - 1) + dfs(grid, x + 1, y) + dfs(grid, x - 1, y);
+        covered[i][j] = true;
+        return 1 +
+                dfs(grid, i + 1, j) +
+                dfs(grid, i - 1, j) +
+                dfs(grid, i, j + 1) +
+                dfs(grid, i, j - 1);
     }
 }
