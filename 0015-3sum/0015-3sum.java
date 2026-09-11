292ad19a6ac62b6b2664f10680248464a4523dd4
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-11 18:39:09 -0500

    Time: 33 ms (70.77%), Space: 58.8 MB (90.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,33 +3,33 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
 
-        for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            
+        for(int i = 0;i<nums.length-2;i++){
+          if(i > 0 && nums[i] == nums[i-1]){
+            continue;
+          }
             int j = i+1;
             int k = nums.length-1;
-            
-            while(j < k){
+
+            while(j <k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while(j < k && nums[j] == nums[j+1]){
+                    while(j<k && nums[j] == nums[j+1]){
                         j++;
                     }
-                    while(j < k && nums[k] == nums[k-1]){
+                    while(j<k && nums[k] == nums[k-1]){
                         k--;
                     }
                     j++;
                     k--;
-                }else if(sum > 0){
+                }else if(sum>0){
                     k--;
                 }else{
                     j++;
                 }
             }
-        } 
-        return res;      
+        }
+
+        return res;
     }
 }
