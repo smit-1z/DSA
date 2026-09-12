41f7abd1d4c745a69746c1c2b9026af7295c924b
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-12 11:46:31 -0500

    Time: 4 ms (56.94%), Space: 42.6 MB (95.48%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,37 +1,35 @@
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        char maxChar = s.charAt(0);
-        int maxCharCount = 0;
-        int size = s.length();
+        Map<Character, Integer> freqMap = new HashMap<>();
+        int maxFreq = 1;
+        char maxFreqChar = s.charAt(0);
 
-        for(char c:s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0) +1);
-            if(maxCharCount < map.get(c)){
-                maxChar = c;
-                maxCharCount = map.get(c);
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+            if (maxFreq < freqMap.get(c)) {
+                maxFreq = freqMap.get(c);
+                maxFreqChar = c;
             }
         }
-      
-        if(maxCharCount > (size+1)/2){
+
+        if(maxFreq > (s.length() + 1)/2){
             return "";
         }
 
         char[] res = new char[s.length()];
+        
         int pos = 0;
-
-        for(int i=0;i<maxCharCount;i++){
-            res[pos] = maxChar;
+        
+        for(int i=0;i<maxFreq;i++){
+            res[pos] = maxFreqChar;
             pos+=2;
         }
 
-        map.put(maxChar,0);
+        freqMap.put(maxFreqChar, 0);
 
-        for(char c:map.keySet()){
-            int freq = map.get(c);
-
-            while(freq-- >0){
-                if(pos >= size){
+        for(char c:freqMap.keySet()){
+            for(int i=0;i<freqMap.get(c);i++){
+                if(pos >= s.length()){
                     pos = 1;
                 }
                 res[pos] = c;
