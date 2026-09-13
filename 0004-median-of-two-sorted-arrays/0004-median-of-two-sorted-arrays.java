1880ba7be69b38a618444018d5b8a5c0446e79bc
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-13 11:32:03 -0500

    Time: 2 ms (45.9%), Space: 48.9 MB (51.88%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,41 +1,37 @@
 class Solution {
-    int p1 = 0;
-    int p2 = 0;
-
+        int p = 0;
+        int q = 0;
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int size = nums1.length + nums2.length;
 
-        if(size % 2 == 0){
-            for(int i=0;i<(size-1)/2;i++){
+        if (size % 2 == 0) {
+            for (int i = 0; i < (size-2)/2; i++) {
                 getNext(nums1,nums2);
             }
-            return (double) ((getNext(nums1,nums2) + getNext(nums1,nums2))/2.0);
+            return  (getNext(nums1,nums2) +  getNext(nums1,nums2))/2.0;
         }else{
-            for(int i=0;i<(size)/2;i++){
-                getNext(nums1,nums2);
+            for(int i=0;i<(size-1)/2;i++){
+               getNext(nums1,nums2);  
             }
-
-            return (double) getNext(nums1,nums2);
+             return getNext(nums1,nums2);
         }
     }
 
     public double getNext(int[] nums1, int[] nums2){
-        if(p1 < nums1.length && p2 < nums2.length){
-            if(nums1[p1] < nums2[p2]){
-                return (double) nums1[p1++];
+        if(p>=0 && q>=0 && p<nums1.length && q<nums2.length){
+            if(nums1[p] < nums2[q]){
+                return nums1[p++];
             }else{
-                return (double) nums2[p2++];
+                return nums2[q++];
             }
         }
 
-         if(p1 < nums1.length){
-            return nums1[p1++];
-         }
 
-         if(p2 < nums2.length){
-            return nums2[p2++];
-         }
-         return 0.0;
-         
+        if(p>=0 && p<nums1.length){
+             return nums1[p++];
+        }else{
+            return nums2[q++];
+        }
+
     }
 }
