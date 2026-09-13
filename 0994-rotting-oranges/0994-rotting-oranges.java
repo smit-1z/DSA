4cd663628a8424a64cfc8b2d9ae97801989afa90
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-12 21:39:23 -0500

    Time: 2 ms (87.95%), Space: 44.3 MB (39.69%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,30 +1,30 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> queue = new LinkedList<>();
-
         int m = grid.length;
         int n = grid[0].length;
         int fresh = 0;
+        Queue<int[]> queue = new LinkedList<>();
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
-                    queue.add(new int[] { i, j });
+                    queue.offer(new int[] { i, j });
                 } else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
-        int[][] dirs = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
-
+        int[][] dirs = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
         int time = 0;
-        while (!queue.isEmpty() && fresh > 0) {
+
+        while (!queue.isEmpty() && fresh>0) {
             int size = queue.size();
 
             for (int i = 0; i < size; i++) {
-                int[] orange = queue.poll();
+                int[] node = queue.poll();
                 for (int[] dir : dirs) {
-                    int x = orange[0] + dir[0];
-                    int y = orange[1] + dir[1];
+                    int x = node[0] + dir[0];
+                    int y = node[1] + dir[1];
 
                     if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 1) {
                         grid[x][y] = 2;
@@ -35,9 +35,6 @@ class Solution {
             }
             time++;
         }
-
-        if (fresh == 0)
-            return time;
-        return -1;
+     return fresh == 0 ? time : -1;
     }
 }
