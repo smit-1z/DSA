ef553147cb7fd35bb4cbddbaa3e720767c35598d
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-16 20:48:37 -0500

    Time: 3 ms (46.49%), Space: 46.2 MB (91.54%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,32 +1,30 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashMap<Integer, HashSet<Integer>> row = new HashMap<>();
-        HashMap<Integer, HashSet<Integer>> col = new HashMap<>();
-        HashMap<Integer, HashSet<Integer>> grid = new HashMap<>();
+        List<HashSet<Character>> rows = new ArrayList<>();
+        List<HashSet<Character>> cols = new ArrayList<>();
+        List<HashSet<Character>> grid = new ArrayList<>();
 
-        for(int i =0;i<9;i++){
-            row.put(i,  new HashSet<>());
-            col.put(i,  new HashSet<>());
-            grid.put(i,  new HashSet<>());
+        for (int i = 0; i < 9; i++) {
+            rows.add(new HashSet<>());
+            cols.add(new HashSet<>());
+            grid.add(new HashSet<>());
         }
 
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-               if(board[i][j] != '.'){
-                 int c = board[i][j] - '0';
-                int gridKey =(i/3 * 3) + j/3;
-
-                if(row.get(i).contains(c) || col.get(j).contains(c) || grid.get(gridKey).contains(c)){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == '.') continue;
+                if (rows.get(i).contains(board[i][j])
+                        || cols.get(j).contains(board[i][j]) || grid.get((i / 3 * 3) + j / 3).contains(board[i][j])) {
                     return false;
                 }
-                
-                row.get(i).add(c);
-                col.get(j).add(c);
-                grid.get(gridKey).add(c);
-               }
+                rows.get(i).add(board[i][j]);
+                cols.get(j).add(board[i][j]);
+                grid.get((i / 3 * 3) + j / 3).add(board[i][j]);
+
             }
         }
 
         return true;
+
     }
 }
