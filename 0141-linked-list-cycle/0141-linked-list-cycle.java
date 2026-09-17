d233f0a7a19ac4d16079cbd0e9f02fbcc72aac31
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-17 10:46:19 -0500

    Time: 0 ms (100%), Space: 47.1 MB (16.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,11 +15,12 @@ public class Solution {
         ListNode fast = head;
 
         while(fast != null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+
             if(slow == fast){
                 return true;
             }
-            slow = slow.next;
-            fast = fast.next.next;
         }
         return false;
     }
