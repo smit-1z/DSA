686ad8937eb3535ff2331c83e98e0c1285b20f34
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-17 10:45:45 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,13 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
+        while(fast.next!= null){
+            if(slow == fast){
+                return true;
+            }
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast)
-                return true;
         }
         return false;
     }
