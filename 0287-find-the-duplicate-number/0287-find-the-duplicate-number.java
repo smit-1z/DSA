313086b1e2cf2c6e8f88cc49825db72a00af9c99
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-18 22:49:00 -0500

    Time: 3 ms (94.58%), Space: 82.9 MB (58.9%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int duplicate = -1;
+
+       for(int i=0;i<nums.length;i++){
+            int cur = Math.abs(nums[i]);
+            if(nums[cur] < 0){
+                duplicate = cur;
+                break;
+            }
+            nums[cur] = -1*nums[cur];
+        }
+
+        return duplicate;
+    }
+}
