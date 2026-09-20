19c944df5e0e36dc4dbc2efa32e3cb2cf711aba8
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-20 10:04:34 -0500

    Time: 5 ms (31.95%), Space: 69.3 MB (92.12%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,8 +1,6 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int sum = 0;
-        int start = 0;
-
+        double sum = 0;
         for(int i=0;i<k;i++){
             sum += nums[i];
         }
@@ -11,7 +9,7 @@ class Solution {
 
         for(int i=k;i<nums.length;i++){
             sum+=nums[i];
-            sum-=nums[start++];
+            sum-=nums[i-k];
             res = Math.max(res, sum);
         }
 
