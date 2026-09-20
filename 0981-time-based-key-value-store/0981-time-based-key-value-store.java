d831c340b06ae88274af134a1f16d87b25ca99fa
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-20 11:20:19 -0500

    Time: 114 ms (96.5%), Space: 109.2 MB (14.88%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,24 +1,39 @@
 class TimeMap {
-    Map<String,TreeMap<Integer,String>> map;
+    Map<String, List<Pair<Integer, String>>> map;
+
     public TimeMap() {
         this.map = new HashMap<>();
     }
-    
+
     public void set(String key, String value, int timestamp) {
-        if(!map.containsKey(key)){
-            map.put(key, new TreeMap<>());
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
         }
-        map.get(key).put(timestamp, value);
+        map.get(key).add(new Pair(timestamp, value));
     }
-    
+
     public String get(String key, int timestamp) {
-        if(!map.containsKey(key)) return "";
+        if (!map.containsKey(key))
+            return "";
 
-        TreeMap<Integer,String> entry = map.get(key);
-        Integer floorKey = entry.floorKey(timestamp);
-        if(floorKey!= null) return entry.get(floorKey); 
+        List<Pair<Integer, String>> entry = map.get(key);
+        
+        String res = "";
 
-       return "";
+        int l = 0;
+        int r = entry.size()-1;
+
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(entry.get(mid).getKey() <= timestamp){
+                res = entry.get(mid).getValue();
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+
+        return res;
     }
 }
 
