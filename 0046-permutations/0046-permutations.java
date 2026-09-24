2c488562718a68a5fb24ee1f6c2491978a69c574
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-23 22:40:35 -0500

    Time: 1 ms (88.17%), Space: 45.2 MB (89.49%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,11 +3,11 @@ class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
         this.res = new ArrayList<>();
-        backTrack(nums,  new ArrayList<>());
+        backTrack(nums,  new ArrayList<>(), new boolean[nums.length]);
         return res;
     }
 
-    public void backTrack(int[] nums,  ArrayList<Integer> list) {
+    public void backTrack(int[] nums,  ArrayList<Integer> list, boolean[] used) {
         if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
             return;
@@ -15,11 +15,12 @@ class Solution {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (!list.contains(nums[i])) {
+            if(used[i]) continue;
+                used[i] = true;
                 list.add(nums[i]);
-                backTrack(nums, list);
+                backTrack(nums, list,used);
+                used[i] = false;
                 list.removeLast();
-            }
         }
     }
 }
