767dc19c149c380e62c13f5b5ba06c0304fea9a9
smit joshi <62090827+smit-1z@users.noreply.github.com>
2026-09-23 19:15:02 -0500

    Time: 3 ms (32.96%), Space: 45.9 MB (32.94%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -4,26 +4,22 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         this.res = new ArrayList<>();
         Arrays.sort(candidates);
-        backTrack(candidates, target, 0, new ArrayList<>(), 0);
+        backTrack(candidates, target, 0, new ArrayList<>());
         return res;
     }
 
-    public void backTrack(int[] candidates, int target, int start, ArrayList<Integer> curr, int curSum) {
-        if (curSum == target) {
+    public void backTrack(int[] candidates, int remaining, int start, ArrayList<Integer> curr) {
+        if (remaining == 0) {
             res.add(new ArrayList<>(curr));
-            
+            return;
         }
 
         for (int i = start; i < candidates.length; i++) {
-            curSum += candidates[i];
-            if(curSum >target){
+            if(candidates[i] > remaining){
                 break;
             }
             curr.add(candidates[i]);
-            if (curSum <= target) {
-                backTrack(candidates, target, i, curr, curSum);
-            }
-            curSum -= candidates[i];
+            backTrack(candidates, remaining-candidates[i], i, curr);
             curr.removeLast();
         }
     }
